f8d9d1d762cc257ee7149ab800f50702f66f2384
Himanshuj0311 <himanshu.masai@gmail.com>
2023-04-28 12:50:55 +0530

    action testing

---
+++ index.js
@@ -0,0 +1,25 @@
+// const express=require("express");
+// const redis=require("ioredis")
+
+// const app=express()
+// app.use(express.json());
+// const client = new redis();
+
+// app.post("/",(req,res)=>{
+//     const data=req.body.data
+//     client.set("mykey", data)
+//     res.send("done")
+// })
+
+// app.get("/All",async(req,res)=>{
+//    // const data=req.body
+//   const val=await  client.get("mykey")
+//   console.log(val)
+//   res.send(val)
+// })
+
+// app.listen(2000,()=>{
+//     console.log("server")
+// })
+const str = "hello"
+console.log(str)
